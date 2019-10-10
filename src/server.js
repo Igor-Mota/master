b3e516226a5825003a83e9c801c48ffed665ab3f
Igor <igormoa999@gmail.com>
2019-10-10 18:21:51 -0300

    so treinando

--- src/server.js
+++ src/server.js
@@ -1,7 +1,30 @@
 const express = require("express");
-
+require("./config/configs")
 const app = express();
 
 
 
-app.listen(3000)
+
+app.use(express.json());
+
+
+
+app.get("/", (req,res) =>{
+    res.json({quale:"quale"})
+})
+
+
+
+app.post("/", (req,res) =>{
+    
+    res.json(req.body)
+})
+
+
+
+
+
+
+app.listen(process.env.PORT, ()=>{
+    console.log("quale")
+})
